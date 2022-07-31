eceb85bbb829258cec6423f5431c66ab8e7a56ca
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-07-31 11:34:32 +0530

    Use Effect.

---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Reminder from "../modules/reminder";
+
+interface ReminderListProps {
+  items: Reminder[];
+}
+function ReminderList({ items }: ReminderListProps) {
+  return (
+    <ul className="list-group">
+      {items.map((item) => (
+        <li key={item.id} className="list-group-item">
+          {item.title}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ReminderList;
